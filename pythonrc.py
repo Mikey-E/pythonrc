02d9627aa4eee47e8b500cf69f9d4d40e9b40285
Michael <michaelelgin1@gmail.com>
2025-05-19 20:51:51 -0600

    note about PermissionError

--- pythonrc.py
+++ pythonrc.py
@@ -1,2 +1,9 @@
+"""
+Might cause a PermissionError if the PYTHONSTARTUP env var points to this file and this file is inside a OneDrive directory.
+In that case, consider making a copy of this in a local directory outside OneDrive, and having PYTHONSTARTUP point to that copy.
+Other solutions may be possible, but would involve
+changing OneDrive settings and I don't want to do that over something where the stakes are so low.
+"""
+
 def q():
 	quit()
